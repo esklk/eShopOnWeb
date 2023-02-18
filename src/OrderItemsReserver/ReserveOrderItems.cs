1b60cf5de5f4c8b07ac6f8adc086fff16b12bfff
Yauheni Sakaliuk <yauheni.sakaliuk@externals.adidas.com>
2023-02-18 17:33:33 +0100

    introduce order items reserver

---
+++ src/OrderItemsReserver/ReserveOrderItems.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace OrderItemsReserver
+{
+    public class ReserveOrderItems
+    {
+        [FunctionName("ReserveOrderItems")]
+        public async Task Run([ServiceBusTrigger("orderitems", Connection = "sbConnection")] string message,
+            [Blob("reservations/{rand-guid}.json", FileAccess.Write, Connection = "bsConnection")] TextWriter blobWriter,
+            ILogger log)
+        {
+            log.LogInformation($"Received a message: {message}");
+
+            await blobWriter.WriteAsync(message);
+        }
+    }
+}
