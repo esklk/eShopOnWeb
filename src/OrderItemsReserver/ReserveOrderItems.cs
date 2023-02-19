0b52cea40df1dceb57e2c86644570706a5df1b1c
Yauheni Sakaliuk <yauheni.sakaliuk@externals.adidas.com>
2023-02-19 17:29:31 +0100

    introduce retries

--- src/OrderItemsReserver/ReserveOrderItems.cs
+++ src/OrderItemsReserver/ReserveOrderItems.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
@@ -7,6 +9,9 @@ namespace OrderItemsReserver
 {
     public class ReserveOrderItems
     {
+        private const int RetryAttempts = 3;
+        private static readonly TimeSpan RetryDelay = TimeSpan.FromSeconds(10);
+
         [FunctionName("ReserveOrderItems")]
         public async Task Run([ServiceBusTrigger("orderitems", Connection = "sbConnection")] string message,
             [Blob("reservations/{rand-guid}.json", FileAccess.Write, Connection = "bsConnection")] TextWriter blobWriter,
@@ -14,7 +19,26 @@ namespace OrderItemsReserver
         {
             log.LogInformation($"Received a message: {message}");
 
-            await blobWriter.WriteAsync(message);
+            int attempt = 1;
+            do
+            {
+                try
+                {
+                    await blobWriter.WriteAsync(message);
+                    break;
+                }
+                catch (Exception)
+                {
+                    if (attempt == RetryAttempts)
+                    {
+                        throw;
+                    }
+
+                    attempt++;
+                    Thread.Sleep(RetryDelay);
+                    log.LogFunctionRetryAttempt(RetryDelay,attempt, 3);
+                }
+            } while (true);
         }
     }
 }
